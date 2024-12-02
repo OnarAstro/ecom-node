75d2af0a1d3000994e1e88bddb74f101024e9dad
Omar Atef <126633818+OnarAstro@users.noreply.github.com>
2024-12-02 14:27:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use('/images', express.static("upload/images"))
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: true,
-    image_url: `http://localhost:${port}/images/${req.file.filename}` || `https://ecom-node-production.up.railway.app/images/${req.file.filename}`,
+    image_url: `https://ecom-node-production.up.railway.app/images/${req.file.filename}` || `http://localhost:${port}/images/${req.file.filename}` 
   })
 })
 
