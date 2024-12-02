746f49114b7f9b3ee7874fb16b17a89bcf332e30
Omar Atef <126633818+OnarAstro@users.noreply.github.com>
2024-12-02 14:55:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use('/images', express.static("upload/images"))
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: true,
-    image_url: `${URL}/images/${req.file.filename}` 
+    image_url: `PORT` 
   })
 })
 
