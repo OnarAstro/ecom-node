d784daebe1ef3ab3ffb8ede4d713abcaea245566
Omar Atef <126633818+OnarAstro@users.noreply.github.com>
2024-12-02 14:05:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -261,9 +261,9 @@ app.post("/getcart", fetchUser, async (req, res) => {
 })
 
 
-app.listen(port, (error) => {
+app.listen(PORT, (error) => {
   if (!error) {
-    console.log("Server is running on port: ", port);
+    console.log("Server is running on port: ", PORT);
   } else {
     console.log("Error: ", error);
   }
